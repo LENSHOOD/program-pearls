1fa1e0d046de5cee9fec448c0ccecc7265356d43
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-04-13 22:00:58 +0800

    feat: provide test file generator

---
+++ src/test/java/TestFileGenerator.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+/**
+ * TestFileGenerator:
+ * @author zhangxuhai
+ * @date 2020/4/12
+*/
+public class TestFileGenerator {
+    public static final Path TEST_FILE_PATH = Paths.get("src", "test", "resources", "test-file");
+    private static final int ITEM_SIZE = 10000000;
+    private static final Random RANDOM = new Random();
+
+    @Test
+    void generate() throws IOException {
+        Files.deleteIfExists(TEST_FILE_PATH);
+
+        int loops = ITEM_SIZE;
+        try(BufferedWriter writer = Files.newBufferedWriter(TEST_FILE_PATH);) {
+            while (loops-- > 0) {
+                writer.write(String.valueOf(RANDOM.nextInt(ITEM_SIZE)));
+                writer.write("\n");
+            }
+        }
+    }
+}
